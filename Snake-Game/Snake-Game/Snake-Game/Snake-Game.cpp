496f3ea770d5d884f0350cc9630c0898e1f8bee6
UIT-20521943 <20521943@gm.uit.edu.vn>
2022-11-18 21:28:57 +0700

    Update Snake-Game.cpp

--- Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
+++ Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
@@ -31,6 +31,24 @@ Point prevTail;
 #define HEIGHT 20
 #define BODY '*'
 #define APPLE 'O'
+void drawBox()
+{
+	for (size_t i = 0; i < WIDTH; i++)
+		cout << '=';
+	gotoxy(0, HEIGHT);
+	for (size_t i = 0; i < WIDTH; i++)
+		cout << '=';
+	for (size_t i = 1; i < HEIGHT; i++)
+	{
+		gotoxy(0, i);
+		cout << '|';
+	}
+	for (size_t i = 1; i < HEIGHT; i++)
+	{
+		gotoxy(WIDTH, i);
+		cout << '|';
+	}
+}
 
   /*
    * Enum
