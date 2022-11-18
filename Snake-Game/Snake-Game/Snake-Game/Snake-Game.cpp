89d4f8a2269bcafa2e80f77a693efc09cdaf11b3
UIT-20521943 <20521943@gm.uit.edu.vn>
2022-11-18 22:10:08 +0700

    Update Snake-Game.cpp

--- Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
+++ Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
@@ -1,57 +1,6 @@
-#include <iostream>
-using namespace std;
 
-/*
- * Import library
- */
-#include <vector>
-#include <windows.h>
-#include <conio.h>
-#include <cstring>
-#include <time.h>
-#include <random>
-
- /*cc
-  * MACRO
-  */
- // Create snake
- void drawSnakePart(Point);
- //hello
-vector<Point> snake = {
-	Point{ WIDTH / 2 + 2, HEIGHT / 2 },
-	Point{ WIDTH / 2 + 1, HEIGHT / 2 },
-	Point{ WIDTH / 2, HEIGHT / 2 },
-	Point{ WIDTH / 2 - 1, HEIGHT / 2 },
-	Point{ WIDTH / 2 - 2, HEIGHT / 2 }
-};
-Direction direction = Direction::right;
-Point apple;
-int score = 0;
-int speed = 300;
-Point prevTail;
-#define WIDTH 40
-#define HEIGHT 20
-#define BODY '*'
-#define APPLE 'O'
 void drawBox()
 {
 	for (size_t i = 0; i < WIDTH; i++)
 		cout << '=';
 	gotoxy(0, HEIGHT);
-	for (size_t i = 0; i < WIDTH; i++)
-		cout << '=';
-	for (size_t i = 1; i < HEIGHT; i++)
-	{
-		gotoxy(0, i);
-		cout << '|';
-	}
-	for (size_t i = 1; i < HEIGHT; i++)
-	{
-		gotoxy(WIDTH, i);
-		cout << '|';
-	}
-}
-
-  /*
-   * Enum
-   */
